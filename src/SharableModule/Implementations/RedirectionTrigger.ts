c6e055e5d1dce22296945da87b6e35db6bfb329b
nohimgodage <nohim.godage@gmail.com>
2023-09-09 22:17:51 +0530

    Code RedirectionTrigger

--- src/SharableModule/Implementations/RedirectionTrigger.ts
+++ src/SharableModule/Implementations/RedirectionTrigger.ts
@@ -3,6 +3,19 @@ import IUser from "../Modals/IUser";
 
 class RedirectionTrigger implements ISendMessageBehaviour{
     onSendMessageClick(userDetails: IUser): void {
+        //Mock the API Call
+        const postApiCall = new Promise(resolve => {
+            console.log(`POST call made for ${userDetails.name}`);
+            setTimeout(() => {
+                resolve(userDetails);
+            }, 1000);
+        });
+
+        //Do the redirection
+        postApiCall.then(() => {
+            window.location.href = `https://medium.com/@nohim.godage`;
+        })
+
     }
 
 }
