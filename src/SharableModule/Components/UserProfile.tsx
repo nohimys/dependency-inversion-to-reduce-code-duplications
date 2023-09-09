306e9cdfac3345e0666138ad33eeb86f4bb9fab0
nohimgodage <nohim.godage@gmail.com>
2023-09-09 20:04:59 +0530

    Implementing User Profile Component with props & Styles

--- src/SharableModule/Components/UserProfile.tsx
+++ src/SharableModule/Components/UserProfile.tsx
@@ -1,14 +1,15 @@
 import {useEffect, useState} from "react";
 import UserApis from "../Utils/UserApis";
 import IUser from "../Modals/IUser";
-import {deepOrange} from "@mui/material/colors";
-import {Avatar, CircularProgress} from "@mui/material";
+import {Avatar, Box, Button, CircularProgress, Typography} from "@mui/material";
 
 type UserProfileProps = {
-    id: number
+    id: number,
+    showImage: boolean,
+    showCompany: boolean
 }
 const UserProfile = (props: UserProfileProps) => {
-    const {id} = props;
+    const {id, showImage, showCompany} = props;
 
     const [user, setUser] = useState<IUser | undefined>(undefined)
 
@@ -24,22 +25,112 @@ const UserProfile = (props: UserProfileProps) => {
             setUser(undefined);
         }
 
-    }, [id])
+    }, [id]);
+
+    const avatarImage = () => {
+        return(
+            <>
+                {
+                    showImage
+                        ?
+                        <Avatar
+                            src="https://facts.net/wp-content/uploads/2023/07/32-facts-about-nicole-maines-1689049554.jpg"
+                            sx={{
+                                width: 100,
+                                height: 100,
+                                margin: 'auto',
+                                marginBottom: '15px'
+                            }}/>
+                        :
+                        <Avatar
+                            sx={{
+                                width: 100,
+                                height: 100,
+                                margin: 'auto',
+                                marginBottom: '15px',
+                                fontSize:'3rem'
+                            }}>{user?.name[0]}</Avatar>
+                }
+            </>
+        );
+    }
 
     if(user === undefined){
         return (
             <CircularProgress color="secondary" />
         );
-    };
+    }
 
+    //window.location.href = "mailto:address@dmail.com";
 
     return(
-        <div style={{width:'50%', margin:"auto", border:'solid 2px red'}}>
-            User Profile: {user?.name}
-            <Avatar
-                sx={{ bgcolor: deepOrange[500],  width: 100, height: 100}}>
-                {user?.name[0]}
-            </Avatar>
+        <div style={{width:'60%', margin:"auto"}}>
+            <Box sx={{
+                boxShadow: 6,
+                padding: '10px',
+                bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#101010' : '#fff'),
+                color: (theme) =>
+                    theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
+            }}>
+                {avatarImage()}
+
+               <div style={{margin:'auto 40px auto 40px'}}>
+                  <div>
+                      <Typography variant="h5" align={'left'}>
+                          {user?.name}
+                      </Typography>
+                      <Typography variant="body1" align={'left'} >
+                          {user?.email}
+                      </Typography>
+                      <Typography variant="body1" align={'left'} >
+                          {user?.phone}
+                      </Typography>
+                      <Typography variant="body1" align={'left'} >
+                          {user?.website}
+                      </Typography>
+                  </div>
+                   <div style={{margin:'20px auto auto auto'}}>
+                       <Typography variant="h6" align={'left'}>
+                           Address:
+                       </Typography>
+                       <Typography variant="body1" align={'left'}>
+                           {user?.address.suite}
+                       </Typography>
+                       <Typography variant="body1" align={'left'}>
+                           {user?.address.street}
+                       </Typography>
+                       <Typography variant="body1" align={'left'}>
+                           {user?.address.city} | {user?.address.zipcode}
+                       </Typography>
+                   </div>
+                   {
+                       showCompany
+                       &&
+                       <div style={{margin: '20px auto auto auto'}}>
+                           <Typography variant="h6" align={'left'}>
+                               Company:
+                           </Typography>
+                           <Typography variant="body1" align={'left'}>
+                               {user?.company.name}
+                           </Typography>
+                           <Typography variant="body1" align={'left'}>
+                               {user?.company.catchPhrase}
+                           </Typography>
+                           <Typography variant="body1" align={'left'}>
+                               {user?.company.bs}
+                           </Typography>
+                       </div>
+                   }
+                   <div id={'button-pane'} style={{textAlign:'right'}}>
+                       <Button
+                           variant="contained"
+                           onClick={() => {window.location.href = "mailto:address@dmail.com"}}
+                       >
+                           Send Message
+                       </Button>
+                   </div>
+               </div>
+            </Box>
         </div>
     );
 }
